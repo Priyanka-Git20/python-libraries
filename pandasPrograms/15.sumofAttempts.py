b8593f73467d23f7e433084c3ab33360ebfcea2f
priyanka <priyanka.chavan1408@gmail.com>
2022-05-07 12:57:07 +0530

    [Priyanka]Add:Calculating the sum of the examination attempts by the students by uding sum().

---
+++ pandasPrograms/15.sumofAttempts.py
@@ -0,0 +1,33 @@
+'''
+@Author : Priyanka
+@Date : 2022-05-05  2:50:00
+@Last Modified by : Priyanka
+@Last Modified Time : 2022-05-05 3:00:00
+@Title : Write a Python program to calculate the sum of the examination attempts by the students.
+'''
+
+import pandas as pd
+import numpy as np
+
+
+def sum_of_attempts():
+    """
+      Description:
+           calculating the sum of the examination attempts by using sum().
+      Parameter:
+           none
+      Return:
+          Returning nothing.
+    """
+    exam_data = {'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew',
+                          'Laura', 'Kevin', 'Jonas'],
+                 'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19],
+                 'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+                 'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes']}
+    labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+    dataframe = pd.DataFrame(exam_data, index=labels)
+    print("The sum of attempts is :",dataframe["attempts"].sum())
+
+
+if __name__ == '__main__':
+    sum_of_attempts()
